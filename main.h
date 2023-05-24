81948e1baf86dee607d7b17cf80003a7bd2d1c74
jihadsheble <jihadsheble@gmail.com>
2023-05-24 06:47:31 -0700

    Add _strcmp function to string_operation file

--- main.h
+++ main.h
@@ -23,5 +23,6 @@ void interactive_mode(int bytes, int id,
 		char *input_buffer, char *argv_buffer[], char *new_arg);
 int _strlen(char *s);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 
 #endif
--- string_operation.c
+++ string_operation.c
@@ -44,3 +44,27 @@ char *_strncpy(char *dest, char *src, int n)
 
 	return (dest);
 }
+
+#include "main.h"
+/**
+ * _strcmp - compare the string values
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: s1[i] - s2[i]
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	a = 0;
+	while (s1[a] != '\0' && s2[a] != '\0')
+	{
+		if (s1[a] != s2[a])
+		{
+			return (s1[a] - s2[a]);
+		}
+	a++;
+	}
+	return (0);
+}
