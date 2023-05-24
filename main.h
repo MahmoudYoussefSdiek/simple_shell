35663ccbc922fd1a9995b6bea6abefb4d869c084
jihadsheble <jihadsheble@gmail.com>
2023-05-24 06:33:12 -0700

    Create string_operation.c file and update main.h

--- main.h
+++ main.h
@@ -21,5 +21,6 @@ void add_bin_prefix(char *argv[], char *new_arg);
 
 void interactive_mode(int bytes, int id,
 		char *input_buffer, char *argv_buffer[], char *new_arg);
+int _strlen(char *s);
 
 #endif
